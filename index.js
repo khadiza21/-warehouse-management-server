ec444c614880311e498f4e1ceff3eeef06283b37
khadiza21 <bibikhadiza474@gmail.com>
2022-05-06 18:38:12 +0600

    delete method create

--- index.js
+++ index.js
@@ -43,6 +43,15 @@ async function run() {
       res.send(result);
     });
 
+
+    //delete
+    app.delete("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await inventoryCollection.deleteOne(query);
+      res.send(result);
+    });
+
    
   } finally {
   }
