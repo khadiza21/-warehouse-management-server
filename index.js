912f89cc603ef38dbeab093ede86a2b4e6d1b920
khadiza21 <bibikhadiza474@gmail.com>
2022-05-07 15:10:03 +0600

    update qunatity

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zsuwx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
@@ -52,28 +51,29 @@ async function run() {
       res.send(result);
     });
 
-      //update user
-      app.put("/inventory/:id", async (req, res) => {
-        const id = req.params.id;
-        const updatedQuantity = req.body;
-        console.log(updatedQuantity);
-        const filter = {_id: ObjectId(id)};
-        const options = { upsert: true };
-        const updatedDoc = {
-          $set: {
-            quantity: updatedQuantity.quantity
-            
-          },
-        };
-        const result = await inventoryCollection.updateOne(filter,updatedDoc,options);
-        res.send(result);
-      });
-
+    //update user
+    app.put("/inventory/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedQuantity = req.body;
+      console.log(updatedQuantity);
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          quantity: updatedQuantity.quantity,
+        },
+      };
+      const result = await inventoryCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
   } finally {
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("server is runnuning and wating for client");
